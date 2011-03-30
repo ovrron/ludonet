5742e0514523f278d9ca32f1e1006d4b02b89939
ronny.overeng@gmail.com <ronny.overeng@gmail.com@5e286520-8841-51ed-d5fc-933d1c4190f6>
2011-03-30 21:59:10 +0000

    eksempel på animasjon av splash samt litt videre på startaktiviteten

--- NetLudo/trunk/src/com/ronny/ludo/LudoSplashActivity.java
+++ NetLudo/trunk/src/com/ronny/ludo/LudoSplashActivity.java
@@ -2,37 +2,64 @@ package com.ronny.ludo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class LudoSplashActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	private ImageView die;
+	private ImageView l;
+	private ImageView u;
+	private ImageView d;
+	private ImageView o;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        startAnimating();
+
+        startDieFrameAnimation(true);
+        MediaPlayer mp = MediaPlayer.create(getBaseContext(),R.raw.viacom2);
+        mp.start();
     }
 
-    /**
-     * Helper method to start the animation on the splash screen
-     */
-    private void startAnimating() {
-        // Fade in top title
-        TextView splashtext = (TextView) findViewById(R.id.textSplash);
-        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate_splashtext);
-        splashtext.startAnimation(rotate);
+    private void startAnimation()
+    {
+    	Animation lAnimation1 = AnimationUtils.loadAnimation(this, R.anim.logoletters_in1);
+    	Animation lAnimation2 = AnimationUtils.loadAnimation(this, R.anim.logoletters_in2);
+    	Animation lAnimation3 = AnimationUtils.loadAnimation(this, R.anim.logoletters_in3);
+    	Animation lAnimation4 = AnimationUtils.loadAnimation(this, R.anim.logoletters_in4);
+    	
+    	l = (ImageView) findViewById(R.id.imageViewL);
+    	u = (ImageView) findViewById(R.id.imageViewU);
+    	d = (ImageView) findViewById(R.id.imageViewD);
+    	o = (ImageView) findViewById(R.id.imageViewO);
+    	
+        l.setVisibility(ImageView.VISIBLE);
+        u.setVisibility(ImageView.VISIBLE);
+        d.setVisibility(ImageView.VISIBLE);
+        o.setVisibility(ImageView.VISIBLE);
+        
+        l.startAnimation(lAnimation1);
+        u.startAnimation(lAnimation2);
+        d.startAnimation(lAnimation3);
+        o.startAnimation(lAnimation4);
+        
 
-        // Transition to Main Menu when bottom title finishes animating
-        rotate.setAnimationListener(new AnimationListener() {
+    
+        lAnimation1.setAnimationListener(new AnimationListener() {
 
             public void onAnimationEnd(Animation animation) {
                 // The animation has ended, transition to the Main Menu screen
-                startActivity(new Intent(LudoSplashActivity.this, LudoStartActivity.class));
-                LudoSplashActivity.this.finish();
+            	startDieFrameAnimation(false);
+            	endAnimation();
             }
 
             public void onAnimationRepeat(Animation animation) {
@@ -42,21 +69,93 @@ public class LudoSplashActivity extends Activity {
             }
         });
     }
+    
+    private void endAnimation()
+    {
+    	Animation lAnimation1 = AnimationUtils.loadAnimation(this, R.anim.logoletters_out1);
+    	Animation lAnimation2 = AnimationUtils.loadAnimation(this, R.anim.logoletters_out2);
+    	Animation lAnimation3 = AnimationUtils.loadAnimation(this, R.anim.logoletters_out3);
+    	Animation lAnimation4 = AnimationUtils.loadAnimation(this, R.anim.logoletters_out4);
+    	
+        
+    	l.startAnimation(lAnimation1);
+    	u.startAnimation(lAnimation2);
+        d.startAnimation(lAnimation3);
+        o.startAnimation(lAnimation4);
+        
+        
+        lAnimation1.setAnimationListener(new AnimationListener() {
 
+            public void onAnimationEnd(Animation animation) {
+                // The animation has ended, transition to the Main Menu screen
+                die.setVisibility(ImageView.INVISIBLE);
+                l.setVisibility(ImageView.INVISIBLE);
+                u.setVisibility(ImageView.INVISIBLE);
+                d.setVisibility(ImageView.INVISIBLE);
+                o.setVisibility(ImageView.INVISIBLE);
+            	startActivity(new Intent(LudoSplashActivity.this, LudoStartActivity.class));
+            	LudoSplashActivity.this.finish();
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
+    }
+    
+
+    private void startDieFrameAnimation(boolean in)
+    {
+        die = (ImageView) findViewById(R.id.imageViewAnim);
+        int res;
+        if(in)
+        {
+        	res = R.drawable.animdice_in;
+        }
+        else
+        {
+        	res = R.drawable.animdice_out;
+        }
+        die.setBackgroundResource(res);
+    	final AnimationDrawable frameAnimation = (AnimationDrawable) die.getBackground();
+//        long frameAnmimationDuration=0;
+//        for(int i = 0; i< frameAnimation.getNumberOfFrames();i++)
+//        {
+//        	frameAnmimationDuration += frameAnimation.getDuration(i);
+//        }
+        die.post(new Runnable()
+        {
+			@Override
+			public void run()
+			{
+				frameAnimation.start();
+			}  		        
+        });
+	}
+
+    
     @Override
     protected void onPause() {
         super.onPause();
         // Stop the animation
-        TextView splashtext = (TextView) findViewById(R.id.textSplash);
-        splashtext.clearAnimation();
+        if(die!=null)
+        	die.clearAnimation();
+        if(l!=null)
+        	l.clearAnimation();
+        if(u!=null)
+        	u.clearAnimation();
+        if(d!=null)
+        	d.clearAnimation();
+        if(o!=null)
+        	o.clearAnimation();        
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Start animating at the beginning so we get the full splash screen experience
-        startAnimating();
+        startAnimation();
     }
-
+    
 }
--- NetLudo/trunk/src/com/ronny/ludo/LudoStartActivity.java
+++ NetLudo/trunk/src/com/ronny/ludo/LudoStartActivity.java
@@ -2,23 +2,43 @@ package com.ronny.ludo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.ronny.ludo.helper.IPAddressHelper;
 
-public class LudoStartActivity extends Activity {
+public class LudoStartActivity extends Activity 
+{
 	
-	private String ipAdress = null;
-	private Button buttonStartSpill = null;
-	private Button buttonSendSMS = null;
+	private String ipAddressCurrent = null;
+	private String ipAddressJoin = null;
+	private EditText editTextIP = null;
+	private Button buttonStartGame = null;
+	private Button buttonInviteJoin = null;
+	private RadioButton radioButtonHostGame = null;
+	private TextView textViewIP = null;
+	private ImageView imageViewPlayerRed = null;
+	private ImageView imageViewPlayerGreen = null;
+	private ImageView imageViewPlayerYellow = null;
+	private ImageView imageViewPlayerBlue = null;
 	
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
         init();
@@ -26,29 +46,104 @@ public class LudoStartActivity extends Activity {
     
     private void init()
     {
-    	TextView textViewIpAddress = (TextView)findViewById(R.id.textIp);
-    	ipAdress = new IPAddressHelper().getLocalIpAddress();
-    	if(ipAdress==null){
-    		textViewIpAddress.setText("Ingen nettverksforbindelse");
-    	}
-    	else{
-    		textViewIpAddress.setText("IP: " + ipAdress);
-    	}
-    	initButtons();
+    	editTextIP = (EditText) findViewById(R.id.editTextIP);
+    	ipAddressCurrent = new IPAddressHelper().getLocalIpAddress();
+    	ipAddressJoin = getResources().getString(R.string.splash_edittext_ip);
+    	buttonInviteJoin = (Button) findViewById(R.id.buttonIP);
+    	buttonStartGame = (Button) findViewById(R.id.buttonStartGame);
+    	radioButtonHostGame = (RadioButton) findViewById(R.id.radioHostGame);
+    	imageViewPlayerRed = (ImageView) findViewById(R.id.imageViewPlayerRed);
+    	imageViewPlayerGreen = (ImageView) findViewById(R.id.imageViewPlayerGreen);
+    	imageViewPlayerYellow = (ImageView) findViewById(R.id.imageViewPlayerYellow);
+    	imageViewPlayerBlue = (ImageView) findViewById(R.id.imageViewPlayerBlue);
+    	
+    	textViewIP = (TextView) findViewById(R.id.textIp); 
+    	initRadioButtonListener();
+    	initButtonListeners();
+    	configureGuiElements();
+    	initEditTextListener();
     }
     
-    private void initButtons()
+    
+    private void initRadioButtonListener()
     {
-    	buttonStartSpill = (Button) findViewById(R.id.buttonStart);
-    	buttonSendSMS = (Button) findViewById(R.id.buttonSendIP);
-    	
-    	if(ipAdress==null)
+
+    	RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroupGameType);
+    	rg.setOnCheckedChangeListener(new OnCheckedChangeListener() 
     	{
-    		buttonStartSpill.setEnabled(false);
-    		buttonSendSMS.setEnabled(false);
+    		public void onCheckedChanged (RadioGroup group, int checkedId)
+    		{
+    			configureGuiElements();
+    		}
+    	});
+    }
+
+    private void configureGuiElements()
+    {
+    	if(radioButtonHostGame.isChecked())
+    	{
+     		ipAddressCurrent = new IPAddressHelper().getLocalIpAddress();
+     		if(ipAddressCurrent==null)
+        	{
+     			editTextIP.setText(getResources().getString(R.string.splash_edittext_no_network));
+        		buttonInviteJoin.setEnabled(false);
+        		buttonStartGame.setEnabled(false);
+        		stopPlayerFrameAnimation(imageViewPlayerRed);
+        	}
+        	else
+        	{
+        		editTextIP.setText(ipAddressCurrent);
+        		buttonInviteJoin.setEnabled(true);
+        		buttonStartGame.setEnabled(true);
+        		startPlayerFrameAnimation(imageViewPlayerRed);
+        		//imageViewPlayerRed.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.player_red_check));
+        	}
+     		editTextIP.setEnabled(false);
+     		buttonInviteJoin.setText(getResources().getString(R.string.splash_button_ip_server));
+     		textViewIP.setText(getResources().getString(R.string.splash_tekst_ip_server));
     	}
+		else
+		{
+     		buttonInviteJoin.setText(getResources().getString(R.string.splash_button_ip_client));
+     		textViewIP.setText(getResources().getString(R.string.splash_tekst_ip_client));
+     		editTextIP.setText(ipAddressJoin);
+     		editTextIP.setEnabled(true);
+    		if(ipAddressCurrent==null)
+    		{
+				buttonInviteJoin.setEnabled(false);
+	    		buttonStartGame.setEnabled(false);
+    		}
+    		else
+    		{
+				buttonInviteJoin.setEnabled(true);
+    		}
+    		stopPlayerFrameAnimation(imageViewPlayerRed);
+		}
+    }
+    
+    private void initEditTextListener()
+    {
+    	editTextIP.setOnKeyListener(new OnKeyListener()
+    	{
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event)
+			{
+				ipAddressJoin = editTextIP.getText().toString();
+				return false;
+			}
+    		
+    	});
+    }
+    
+    private void initButtonListeners()
+    {
+    	if(ipAddressCurrent==null)
+    	{
+
+    	}    	
     	
-    	buttonStartSpill.setOnClickListener(new OnClickListener()
+    	buttonStartGame.setOnClickListener(new OnClickListener()
 		{
 			
 			public void onClick(View v)
@@ -58,29 +153,69 @@ public class LudoStartActivity extends Activity {
 			}
 		});
     
-    	buttonSendSMS.setOnClickListener(new OnClickListener()
-		{
+    	buttonInviteJoin.setOnClickListener(new OnClickListener()
+    	{
 			public void onClick(View v)
 			{
-				//TODO legg inn tekst som skal sendes i strings.xml
-				//TODO lage mulighet for en venneliste med telefonnr som man kan velge fra
-				//TODO kanskje mulighet for å sende epost også
-				String msgText = "NN inviterer deg til et spennende slag ludo. IP adresse = " + ipAdress;
-		    	//Intent msg = new Intent(Intent.ACTION_SEND);
-		    	//msg.setType("text/plain");
-		    	//msg.putExtra("sms_body", msgText);
-		    	//msg.putExtra(Intent.EXTRA_TEXT, "email body");
-		    	//msg.putExtra(Intent.EXTRA_SUBJECT, "email subject");
-		        //startActivity(msg);		
-		        //startActivity(Intent.createChooser(msg, "Velg sms"));
-		        
-		        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-		        sendIntent.putExtra("sms_body", msgText);
-//		        String tlfno = "90177797, 99001155, 1234678";
-//		        sendIntent.putExtra("address", tlfno);
-		        sendIntent.setType("vnd.android-dir/mms-sms");
-		        startActivity(sendIntent);  
+				//Send invitasjon
+				if(radioButtonHostGame.isChecked())
+				{
+				
+					//TODO legg inn tekst som skal sendes i strings.xml
+					//TODO lage mulighet for en venneliste med telefonnr som man kan velge fra
+					//TODO kanskje mulighet for å sende epost også
+					String msgText = ipAddressCurrent + " "+ getResources().getString(R.string.splash_invite_text);
+			    	//Intent msg = new Intent(Intent.ACTION_SEND);
+			    	//msg.setType("text/plain");
+			    	//msg.putExtra("sms_body", msgText);
+			    	//msg.putExtra(Intent.EXTRA_TEXT, "email body");
+			    	//msg.putExtra(Intent.EXTRA_SUBJECT, "email subject");
+			        //startActivity(msg);		
+			        //startActivity(Intent.createChooser(msg, "Velg sms"));
+			        
+			        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+			        sendIntent.putExtra("sms_body", msgText);
+	//		        String tlfno = "90177797, 99001155, 1234678";
+	//		        sendIntent.putExtra("address", tlfno);
+			        sendIntent.setType("vnd.android-dir/mms-sms");
+			        startActivity(sendIntent);
+				}
+				
+				//kople til et eksisterende spill
+				else
+				{
+					//TODO
+					
+				}
 			}
 		});
+    }
+    
+    private void startPlayerFrameAnimation(ImageView... players)
+    {
+    	for(ImageView player:players)
+    	{
+    		player.setBackgroundResource(R.drawable.playeranim);
+        	final AnimationDrawable frameAnimation = (AnimationDrawable) player.getBackground();
+            player.post(new Runnable()
+            {
+    			@Override
+    			public void run()
+    			{
+    				frameAnimation.start();
+    			}  		        
+            });
+    		
+    	}
+	}
+    
+    private void stopPlayerFrameAnimation(ImageView... players)
+    {
+       	for(ImageView player:players)
+    	{
+       		player.setBackgroundDrawable(null);
+//       		AnimationDrawable frameAnimation = (AnimationDrawable) player.getBackground();
+//       		frameAnimation.stop();
+    	}
     }
 }
