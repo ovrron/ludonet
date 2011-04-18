1b9c8c260c437fa15bb5f043bd659576587055df
karl.erik.moberg@gmail.com <karl.erik.moberg@gmail.com@5e286520-8841-51ed-d5fc-933d1c4190f6>
2011-04-18 23:02:56 +0000

    ny metode getPieceActionList

--- NetLudo/trunk/src/com/ronny/ludo/rules/IRules.java
+++ NetLudo/trunk/src/com/ronny/ludo/rules/IRules.java
@@ -2,12 +2,14 @@ package com.ronny.ludo.rules;
 
 import java.util.List;
 import com.ronny.ludo.model.IPiece;
+import com.ronny.ludo.model.PieceAction;
 import com.ronny.ludo.model.PlayerColor;
 
 public interface IRules
 {
 	public boolean isLegalMove(IPiece piece, int eyes);
 	public boolean handleMove(IPiece piece, List<IPiece> pieces);
+	public List<PieceAction> getPieceActionList(IPiece piece, List<IPiece> pieces);
 	public PlayerColor getNextPlayerColor(PlayerColor currentPlayerColor, int eyes);
 	public void setTakeOffNumbers(int... takeOffNumbers);
 	public void setReRollNumbers(int... reRollNumbers);
