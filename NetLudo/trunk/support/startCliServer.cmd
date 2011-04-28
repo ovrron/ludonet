219dc9e15607f46cedb042db9859ca06da3b6aa6
meitemarken <meitemarken@5e286520-8841-51ed-d5fc-933d1c4190f6>
2011-04-28 05:56:31 +0000

    Bare en liten oppdatering....

--- NetLudo/trunk/support/startCliServer.cmd
+++ NetLudo/trunk/support/startCliServer.cmd
@@ -6,4 +6,7 @@ sleep 10
 set /p srcemu= Serial til server-emulator (tallet xxxx:tjener)
 echo %srcemu%
 
-rem adb -s emulator-%srcemu% forward tcp:11700 tcp:11700
+echo Her er det du bør skrive for routing.  (Host ip er da 10.0.2.2)
+echo (copy og kjør selv...)
+rem echo adb -s emulator-5554 forward tcp:11700 tcp:11700
+echo adb -s emulator-%srcemu% forward tcp:11700 tcp:11700
